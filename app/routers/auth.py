f65a96c20cf8259bb834963792ecb38ae94c3c5f
Eduardo Ortiz <eduardo@kuadra.cl>
2026-02-10 00:56:04 -0300

    fix auth / security

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, Request, Form
+from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+
+from sqlalchemy.orm import Session
+
+from app.db import get_db
+from app.models.user import User
+from app.security import verify_password
 
 router = APIRouter(prefix="/auth")
 templates = Jinja2Templates(directory="app/templates")
@@ -13,16 +19,23 @@ def login_page(request: Request):
 
 @router.post("/login")
 def login_action(
-    request: Request, username: str = Form(...), password: str = Form(...)
+    request: Request,
+    username: str = Form(...),
+    password: str = Form(...),
+    db: Session = Depends(get_db),
 ):
-    if username == "admin" and password == "1234":
-        request.session["user"] = "admin"
-        return RedirectResponse("/admin/dashboard", status_code=302)
+    user = db.query(User).filter(User.username == username).first()
 
-    return templates.TemplateResponse(
-        "admin/login.html",
-        {"request": request, "error": "Credenciales incorrectas"},
-    )
+    if not user or not verify_password(password, user.password_hash):
+        return templates.TemplateResponse(
+            "admin/login.html",
+            {"request": request, "error": "Credenciales incorrectas"},
+        )
+
+    request.session["user_id"] = user.id
+    request.session["user_email"] = user.username
+
+    return RedirectResponse("/admin/dashboard", status_code=302)
 
 
 @router.get("/logout")
