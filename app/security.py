f95539e8ca8358153a022489f23e5332c6086c41
Eduardo Ortiz <eduardo@kuadra.cl>
2026-02-10 17:25:48 -0300

    fix bcrypt 72 bytes password

--- app/security.py
+++ app/security.py
@@ -4,8 +4,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
+    # bcrypt soporta máximo 72 bytes
+    password = password.strip().encode("utf-8")[:72]
     return pwd_context.hash(password)
 
 
-def verify_password(password: str, password_hash: str) -> bool:
-    return pwd_context.verify(password, password_hash)
+def verify_password(password: str, hashed: str) -> bool:
+    password = password.strip().encode("utf-8")[:72]
+    return pwd_context.verify(password, hashed)
